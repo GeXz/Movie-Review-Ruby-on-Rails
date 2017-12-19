4e356681fef2b12c25f0bbcbb40718410a36bc80
GeXz <mg.comp4@gmail.com>
2017-12-19 19:49:16 +0100

    Installing the paperclip gem, Add image upload field to the New Movie form and Edit Movie form, Include movie image for each movie on the homepage, Layout movies on the homepage, Layout movie show page

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -52,7 +52,7 @@ class MoviesController < ApplicationController
 	private 
 
 		def movie_params
-			params.require(:movie).permit(:title, :description, :cast, :category_id)
+			params.require(:movie).permit(:title, :description, :cast, :category_id, :movie_img)
 		end
 
 		def find_movie
